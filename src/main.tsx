c7e969ac44b0402c56ff0310f08a6fd7a2c9b140
Mmesomadavid <chukwunoyelummesoma1@gmail.com>
2025-09-08 11:55:22 +0100

    removed base name

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/Mmesoma-david">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
